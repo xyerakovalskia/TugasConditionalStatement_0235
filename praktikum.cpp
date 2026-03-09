d0f959297f055d9fa81bbd1f6fcc7e8ad4de8618
xyerakovalskia <xyera123987@gmail.com>
2026-03-09 19:27:13 +0700

    bagian_2

--- praktikum.cpp
+++ praktikum.cpp
@@ -1,2 +1,10 @@
 #include <iostream>
 using namespace std;
+
+// Prosedur untuk memasukkan data
+void inputData(float &berat, float &tinggi) {
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
