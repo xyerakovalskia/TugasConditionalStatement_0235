6e850712d3eac9dd9f89b8a6df050a4b3cab6303
xyerakovalskia <xyera123987@gmail.com>
2026-03-09 19:29:09 +0700

    bagian_4

--- praktikum.cpp
+++ praktikum.cpp
@@ -13,3 +13,19 @@ void inputData(float &berat, float &tinggi) {
 float hitungBMI(float berat, float tinggi) {
     return berat / (tinggi * tinggi);
 }
+
+// Fungsi untuk menentukan status BMI
+string statusBMI(float bmi) {
+    if (bmi < 18.5) {
+        return "Berat Badan Kurang";
+    } 
+    else if (bmi < 25) {
+        return "Berat Badan Normal";
+    } 
+    else if (bmi < 30) {
+        return "Berat Badan Kelebihan";
+    } 
+    else {
+        return "Obesitas";
+    }
+}
